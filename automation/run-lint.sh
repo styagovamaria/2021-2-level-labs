32c025e575fd3db89191f5a0ed861b95af1d489c
Andrej Kashchikhin <43414784+WhiteJaeger@users.noreply.github.com>
2021-09-10 06:33:58 +0300

    [CI] Setup CI based on GitHub Workflow (#2)

---
+++ automation/run-lint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export PYTHONPATH="$(pwd)/lab_1:$(pwd)/lab_2:$(pwd)/lab_3:$(pwd)/lab_4:$(pwd):${PYTHONPATH}"
+
+echo -e '\n'
+echo 'Running lint check...'
+LABS=$(cat automation/labs.txt)
+
+for lab in $LABS; do
+	echo "Running lint for lab #${lab}"
+
+  TARGET_SCORE=$(cat lab_"${lab}"/target_score.txt)
+
+	lint_output=$(python3 -m pylint lab_"${lab}")
+  python3 automation/check_lint.py --lint-output "$lint_output" --target-score $TARGET_SCORE
+done
---
+++ automation/run-tests.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+export PYTHONPATH="$(pwd)/lab_1:$(pwd)/lab_2:$(pwd)/lab_3:$(pwd)/lab_4:$(pwd):${PYTHONPATH}"
+echo "Running tests..."
+
+FAILED=0
+LABS=$(cat automation/labs.txt)
+
+echo "Current scope: $LABS"
+
+for lab in $LABS; do
+	echo "Running tests for lab #${lab}"
+
+	TARGET_SCORE=$(cat lab_"${lab}"/target_score.txt)
+
+	TARGET_TESTS_FILE=automation/lab_"${lab}"/target_tests_"${TARGET_SCORE}".txt
+
+	TARGET_TESTS=$(cat ${TARGET_TESTS_FILE})
+
+	if [[ -z "${TARGET_TESTS// }" ]]; then
+	  echo "No tests provided. Skipping."
+	  continue
+	fi
+
+	while read test_file_name || [[ -n $test_file_name ]]
+	do
+	  echo "Running tests from $test_file_name"
+	  if ! python3 -m unittest lab_"${lab}"/$test_file_name;  then
+    	FAILED=1
+	  fi
+	done <<< "$(cat "$TARGET_TESTS_FILE")"
+
+done
+
+if [[ ${FAILED} -eq 1 ]]; then
+	echo "Tests failed."
+	exit 1
+fi
+
+echo "Tests passed."
