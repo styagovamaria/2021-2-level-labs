93e5d46e0a6f81902a879c4b5fa9c801dc754981
Styagova Maria <90442713+styagovamaria@users.noreply.github.com>
2021-10-04 18:46:43 +0300

    Update start.py

--- lab_1/start.py
+++ lab_1/start.py
@@ -36,3 +36,20 @@ if __name__ == '__main__':
     profiles_10 = [profile_en_10, profile_de_10, profile_la_10]
     # we may save profiles in json file
     main.save_profile(unknown_profile)
+
+    RESULT = main.detect_language_advanced(unknown_profile, profiles_10, [], TOP_N)
+
+    # profile_en_8 = main.create_language_profile("en", en_text, [])
+    # profile_de_8 = main.create_language_profile("de", de_text, [])
+    # profile_la_8 = main.create_language_profile("la", la_text, [])
+    # profiles_8 = [profile_en_8, profile_de_8, profile_la_8]
+    # RESULT_8 = main.detect_language_advanced(unknown_profile, profiles_8, [], TOP_N)
+
+    # if RESULT_10 == RESULT_8:
+    # print("Great!", RESULT_10, "==", RESULT_8)
+    # else:
+    # print("Something is wrong!")
+
+    # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
+    # assert RESULT, 'Detection not working'
+    assert EXPECTED == RESULT, 'Detection not working'
