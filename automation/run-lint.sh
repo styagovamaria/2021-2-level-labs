57ff185830b80ca1dfe51875976f8fbdf4d9ac4a
Andrej Kashchikhin <43414784+WhiteJaeger@users.noreply.github.com>
2021-10-05 13:04:19 +0300

    [CI] Added explicit pylint configuration (#77)

--- automation/run-lint.sh
+++ automation/run-lint.sh
@@ -4,6 +4,7 @@ export PYTHONPATH="$(pwd)/lab_1:$(pwd)/lab_2:$(pwd)/lab_3:$(pwd)/lab_4:$(pwd):${
 
 echo -e '\n'
 echo 'Running lint check...'
+
 LABS=$(cat automation/labs.txt)
 
 for lab in $LABS; do
@@ -11,6 +12,6 @@ for lab in $LABS; do
 
   TARGET_SCORE=$(cat lab_"${lab}"/target_score.txt)
 
-	lint_output=$(python3 -m pylint lab_"${lab}")
+	lint_output=$(python3 -m pylint --rcfile=automation/.pylintrc lab_"${lab}")
   python3 automation/check_lint.py --lint-output "$lint_output" --target-score $TARGET_SCORE
 done
