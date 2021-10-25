d2e446cd3eb2f3cce6a8f35a7c46a685d0468aa7
Styagova Maria <90442713+styagovamaria@users.noreply.github.com>
2021-10-25 18:05:39 +0300

    Update start.py

--- lab_2/start.py
+++ lab_2/start.py
@@ -13,6 +13,9 @@ PATH_TO_LAB_FOLDER = os.path.dirname(os.path.abspath(__file__))
 PATH_TO_PROFILES_FOLDER = os.path.join(PATH_TO_LAB_FOLDER, 'profiles')
 PATH_TO_DATASET_FOLDER = os.path.join(PATH_TO_LAB_FOLDER, 'dataset')
 
+
+# C:\\Users\\krichevskiy\\Downloads\\2021-2-level-labs-main\\2021-2-level-labs-main\\lab_2\\profiles
+
 if __name__ == '__main__':
 
     with open(os.path.join(PATH_TO_PROFILES_FOLDER, 'eng.txt'),
@@ -62,11 +65,14 @@ if __name__ == '__main__':
         known_text_corpus.append(main.remove_stop_words(main.tokenize(lat_text), STOP_WORDS))
         language_labels.append('lat')
     # get language_profiles
+
+
+    #print(mock.called)
     language_profiles = main.get_language_profiles(known_text_corpus, language_labels)
 
     # get known_text_vectors
     known_text_vectors = [main.get_text_vector(text, language_profiles) for text in known_text_corpus]
-    print(known_text_vectors)
+    #print(known_text_vectors)
     # work with unknown texts
     for unk_text in UNKNOWN_SAMPLES:
         # tokenize unknown text and remove stop words in it
@@ -78,7 +84,7 @@ if __name__ == '__main__':
                                                                    known_text_vectors,
                                                                    language_labels,
                                                                    KNN)
-        print(prediction_unknown_languages)
+        #print(prediction_unknown_languages)
         # get language_labels_counts of unknown texts and check the result
         RESULT.append(prediction_unknown_languages[0])
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
