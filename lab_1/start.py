5cd5a3dce66fe177a47d0d580d13994f401743fc
Andrej Kashchikhin <43414784+WhiteJaeger@users.noreply.github.com>
2021-11-12 08:47:57 +0300

    Provide a more meaningful name

--- lab_1/start.py
+++ lab_1/start.py
@@ -11,8 +11,8 @@ PATH_TO_PROFILES_FOLDER = os.path.join(PATH_TO_LAB_FOLDER, 'profiles')
 
 if __name__ == '__main__':
 
-    with open(os.path.join(PATH_TO_TEXTS_FOLDER, 'en.txt'), 'r', encoding='utf-8') as file_to_read:
-        en_text = file_to_read.read()
+    with open(os.path.join(PATH_TO_TEXTS_FOLDER, 'en.txt'), 'r', encoding='utf-8') as file_to_read_en:
+        en_text = file_to_read_en.read()
 
     with open(os.path.join(PATH_TO_TEXTS_FOLDER, 'de.txt'), 'r', encoding='utf-8') as file_to_read:
         de_text = file_to_read.read()
