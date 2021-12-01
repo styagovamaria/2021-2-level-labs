cc3fc5df402a8baca1ff8cfcd02f8d3bdf1fa694
Styagova Maria <90442713+styagovamaria@users.noreply.github.com>
2021-12-01 20:34:25 +0300

    Update start.py

--- lab_3/start.py
+++ lab_3/start.py
@@ -30,6 +30,7 @@ if __name__ == '__main__':
     # EXPECTED_LANGUAGE = ?
     # EXPECTED_MIN_DISTANCE = ?
 
+    
     RESULT = ''
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
     assert RESULT, 'Detection not working'
