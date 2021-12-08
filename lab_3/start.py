1c371f5bf0eaaa02b65249ae2888bd22d0620574
Styagova Maria <90442713+styagovamaria@users.noreply.github.com>
2021-12-09 02:08:46 +0300

    test final3

--- lab_3/start.py
+++ lab_3/start.py
@@ -13,8 +13,8 @@ if __name__ == '__main__':
     GERMAN_SAMPLE = "Zwei Begriffe, die nicht unbedingt zueinander passen,\
      am Arbeitsplatz schon mal gar nicht."
     UNKNOWN_SAMPLE = "Helium is material."
-    SECRET_SAMPLE = """ Некој е болен и тој не е слободен. Dлетува гол во дупка од мраз.
-     И пее, а плаче од болка. Дали е ова контраст, можеби – живот?"""
+    SECRET_SAMPLE = """ Некој е болен и тој не е слободен. Dлетува гол во дупка од мраз.\
+И пее, а плаче од болка. Дали е ова контраст, можеби – живот?"""
 
     # score 6, params: k = 5, trie_level = 2
     # predict UNKNOWN_SAMPLE
